40c000065be39c7fd44cae607999dd9cd52177ff
Pascalle Schipper <pascalle.schipper@wearetriple.com>
2022-05-23 10:06:10 +0200

    add arm nest-command

---
+++ arm-tool/Common/FileHandlers/FileWriter.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace armtool.Common.FileHandlers;
+
+internal class FileWriter
+{
+    public void WriteJSONToFile(object template, string location)
+    {
+        // writes json object to provided location
+        string jsonString = JsonConvert.SerializeObject(template,
+            Formatting.Indented,
+            new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+        File.WriteAllText(location, jsonString);
+    }
+}
---
+++ arm-tool/Program.cs
@@ -0,0 +1,47 @@
+﻿using armtool.Commands;
+using armtool.Common;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace armtool;
+
+internal class Program
+{
+    static int Main(string[] args)
+    {
+
+        var app = new CommandLineApplication()
+        {
+            Name = GlobalConstants.AppShortName,
+            FullName = GlobalConstants.AppLongName,
+            Description = GlobalConstants.AppDescription
+        };
+
+        app.Command("nest", command =>
+        {
+            command.Name = GlobalConstants.NestName;
+            command.Description = GlobalConstants.NestDescription;
+
+            var inputFilesOption = command.Option("--inputFile <inputFile>", "ARM template input", CommandOptionType.MultipleValue);
+            var outputFileOption = command.Option("--outputFile <outputFile>", "Nested ARM template output", CommandOptionType.SingleValue);
+
+            command.OnExecute(() =>
+            {
+                var exitCode = new NestCommand(inputFilesOption.Values, outputFileOption.Value()).Run();
+
+                return exitCode;
+            });
+        });
+
+        try
+        {
+            return app.Execute(args);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            Console.Out.WriteLine(e.ToString());
+        }
+
+        return 1;
+    }
+}
